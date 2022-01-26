9ad5c3fdbde0b90e32e761c2caa2d2e0733dd7dd
unknown <aigiz.iskuzhin@yandex.ru>
2022-01-26 15:02:17 +0500

    added command realization

---
+++ beta/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,25 @@
+﻿using beta.Infrastructure.Commands.Base;
+using System;
+
+namespace beta.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter)
+        {
+            if (!CanExecute(parameter)) return;
+            _Execute(parameter);
+        }
+    }
+}
