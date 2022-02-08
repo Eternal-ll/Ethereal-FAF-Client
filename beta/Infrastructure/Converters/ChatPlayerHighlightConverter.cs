399f72834782501cd929a9e5129ff284e7177cf6
unknown <aigiz.iskuzhin@yandex.ru>
2022-02-08 15:11:07 +0500

    Added converter for highlighting mentions in chat

---
+++ beta/Infrastructure/Converters/ChatPlayerHighlightConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+
+namespace beta.Infrastructure.Converters
+{
+    internal class ChatPlayerHighlightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return false;
+            var textBlock = (TextBlock)value;
+            var inlines = textBlock.Inlines;
+
+            var login = Properties.Settings.Default.PlayerNick;
+            for (int i = 0; i < inlines.Count; i++)
+            {
+                var enumrator = inlines.GetEnumerator();
+                while (enumrator.MoveNext())
+                {
+                    var inline = enumrator.Current;
+                    if (inline is InlineUIContainer ui)
+                    {
+                        if (ui.Child is Button btn)
+                        {
+                            if (btn.Content as string == login)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
