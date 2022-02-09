20b7447442302410bba000ba5abd9a4b8aa019ff
unknown <aigiz.iskuzhin@yandex.ru>
2022-02-09 16:43:58 +0500

    Added some converters

---
+++ beta/Infrastructure/Converters/IsNullConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace beta.Infrastructure.Converters
+{
+    internal class IsNullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value == null;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
