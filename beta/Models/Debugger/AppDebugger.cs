897adfbf0bf1b018e9c2d1943045f17de6ae8a9a
unknown <aigiz.iskuzhin@yandex.ru>
2022-03-09 14:37:33 +0500

    clear reference

--- beta/Models/Debugger/AppDebugger.cs
+++ beta/Models/Debugger/AppDebugger.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using beta.Views.Windows;
+﻿using beta.Views.Windows;
 using System.Diagnostics;
 
 namespace beta.Models.Debugger
