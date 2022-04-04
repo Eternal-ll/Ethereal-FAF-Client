5d2c78070e83aec2b8ad70008a79af2c4d312069
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2022-04-04 13:34:35 +0500

    Removed WinForms dependencies

--- beta/Infrastructure/Utils/ImageTools.cs
+++ beta/Infrastructure/Utils/ImageTools.cs
@@ -1,48 +1,11 @@
 ﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
-using System.Windows;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace beta.Infrastructure.Utils
 {
     internal static class ImageTools
     {
-
-        // Bitmap --> BitmapImage
-        public static BitmapImage BitmapToBitmapImage(Bitmap bitmap)
-        {
-            using MemoryStream stream = new MemoryStream();
-            bitmap.Save(stream, ImageFormat.Png); // Pit: When the format is Bmp, no transparency
-
-            stream.Position = 0;
-            BitmapImage result = new BitmapImage();
-            result.BeginInit();
-            // According to MSDN, "The default OnDemand cache option retains access to the stream until the image is needed."
-            // Force the bitmap to load right now so we can dispose the stream.
-            result.CacheOption = BitmapCacheOption.OnLoad;
-            result.StreamSource = stream;
-            result.EndInit();
-            result.Freeze();
-            return result;
-        }
-
-        // BitmapImage --> Bitmap
-        public static Bitmap BitmapImageToBitmap(BitmapImage bitmapImage)
-        {
-            // BitmapImage bitmapImage = new BitmapImage(new Uri("../Images/test.png", UriKind.Relative));
-
-            using MemoryStream outStream = new MemoryStream();
-            BitmapEncoder enc = new BmpBitmapEncoder();
-            enc.Frames.Add(BitmapFrame.Create(bitmapImage));
-            enc.Save(outStream);
-            Bitmap bitmap = new Bitmap(outStream);
-
-            return new Bitmap(bitmap);
-        }
-
         // RenderTargetBitmap --> BitmapImage
         public static BitmapImage ConvertRenderTargetBitmapToBitmapImage(RenderTargetBitmap wbm)
         {
@@ -85,23 +48,6 @@ namespace beta.Infrastructure.Utils
             return bitmapImage;
         }
 
-
-        // ImageSource --> Bitmap
-        public static Bitmap ImageSourceToBitmap(ImageSource imageSource)
-        {
-            BitmapSource m = (BitmapSource)imageSource;
-
-            Bitmap bmp = new Bitmap(m.PixelWidth, m.PixelHeight, System.Drawing.Imaging.PixelFormat.Format32bppPArgb); // Pit: Select Format32bppRgb will not have transparency
-
-            BitmapData data = bmp.LockBits(
-            new Rectangle(System.Drawing.Point.Empty, bmp.Size), ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
-
-            m.CopyPixels(Int32Rect.Empty, data.Scan0, data.Height * data.Stride, data.Stride);
-            bmp.UnlockBits(data);
-
-            return bmp;
-        }
-
         // BitmapImage --> byte[]
         public static byte[] BitmapImageToByteArray(BitmapImage bmp)
         {
@@ -136,23 +82,6 @@ namespace beta.Infrastructure.Utils
             image.EndInit();
             image.Freeze();
             return image;
-        }
-        public static Bitmap ConvertByteArrayToBitmap(byte[] bytes)
-        {
-            Bitmap img = null;
-            try
-            {
-                if (bytes is not null && bytes.Length != 0)
-                {
-                    MemoryStream ms = new MemoryStream(bytes);
-                    img = new Bitmap(ms);
-                }
-            }
-            catch (Exception ex)
-            {
-
-            }
-            return img;
         }
     }
 }
--- beta/Views/Modals/SelectPathToGameView.xaml.cs
+++ beta/Views/Modals/SelectPathToGameView.xaml.cs
@@ -1,42 +1,69 @@
 ﻿using ModernWpf.Controls;
+using System.ComponentModel;
 using System.IO;
 using System.Windows.Controls;
 
 namespace beta.Views.Modals
 {
-    public partial class SelectPathToGameView : UserControl
+    public partial class SelectPathToGameView : UserControl, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
         private readonly ContentDialog Dialog;
         public SelectPathToGameView(ContentDialog dialog)
         {
             InitializeComponent();
+            DataContext = this;
             Dialog = dialog;
         }
 
+        #region Path
+        private string _Path;
+        public string Path
+        {
+            get => Path;
+            set
+            {
+                _Path = value;
+                PropertyChanged?.Invoke(this, new(nameof(Path)));
+
+                if (Directory.Exists(Path))
+                {
+                    if (File.Exists(Path + "\\bin\\SupremeCommander.exe"))
+                    {
+                        ErrorTextBlock.Text = "Game found";
+                        Dialog.Content = null;
+                        Dialog.Hide();
+                    }
+                }
+                ErrorTextBlock.Text = "Game not found";
+            }
+        }
+        #endregion
+
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            System.Windows.Forms.FolderBrowserDialog dlg = new();
+            // TODO
 
-            var result = dlg.ShowDialog();
-            var error = "Can`t find Bin\\ForgedAlliance.exe in selected folder";
-            if (result == System.Windows.Forms.DialogResult.OK)
-            {
-                if (File.Exists(dlg.SelectedPath + "\\bin\\SupremeCommander.exe"))
-                {
-                    Properties.Settings.Default.PathToGame = dlg.SelectedPath;
+            //var result = dlg.ShowDialog();
+            //var error = "Can`t find Bin\\ForgedAlliance.exe in selected folder";
+            //if (result == System.Windows.Forms.DialogResult.OK)
+            //{
+            //    if (File.Exists(dlg.SelectedPath + "\\bin\\SupremeCommander.exe"))
+            //    {
+            //        Properties.Settings.Default.PathToGame = dlg.SelectedPath;
 
-                    ((Button)sender).Click -= Button_Click;
-                    dlg.Dispose();
+            //        ((Button)sender).Click -= Button_Click;
+            //        dlg.Dispose();
 
-                    Dialog.Content = null;
-                    Dialog.Hide();
-                }
-                else ErrorTextBlock.Text = error;
-            }
-            else
-            {
-                ErrorTextBlock.Text = error;
-            }
+            //        Dialog.Content = null;
+            //        Dialog.Hide();
+            //    }
+            //    else ErrorTextBlock.Text = error;
+            //}
+            //else
+            //{
+            //    ErrorTextBlock.Text = error;
+            //}
         }
     }
 }
