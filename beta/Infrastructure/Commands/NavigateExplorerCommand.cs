10b5159580ac03f64e2b4b3649c9ee748c5b8c2b
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2022-04-04 18:56:59 +0500

    Added command for opening specific directory

---
+++ beta/Infrastructure/Commands/NavigateExplorerCommand.cs
@@ -0,0 +1,47 @@
+﻿using beta.Infrastructure.Commands.Base;
+using beta.Properties;
+using System;
+using System.Diagnostics;
+
+namespace beta.Infrastructure.Commands
+{
+    internal class NavigateExplorerCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter)
+        {
+            if (parameter is null) return;
+
+            var args = string.Empty;
+
+            var user = Environment.UserName;
+
+            switch (parameter.ToString().ToLower())
+            {
+                case "maps":
+                    args = " " + Settings.Default.PathToMaps;
+                    break;
+                case "mods":
+                    args = " " + Settings.Default.PathToMods;
+                    break;
+                case "game":
+                    args = Settings.Default.PathToGame;
+                    break;
+                case "patch":
+                    return;
+                    break;
+                default: return;
+            }
+
+            args = args.Replace("%username%", user);
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "explorer.exe",
+                Arguments = args,
+                UseShellExecute = true,
+            });
+        }
+    }
+}
