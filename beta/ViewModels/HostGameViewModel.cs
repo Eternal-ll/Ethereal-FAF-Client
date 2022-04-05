e7434be4e4b603fe9e2ed57811594a5936788fa7
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2022-04-05 15:20:51 +0500

    Added DataTemplate for password and host game. Changed bg of ContentDialog

---
+++ beta/ViewModels/HostGameViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace beta.ViewModels
+{
+    internal class HostGameViewModel : Base.ViewModel
+    {
+        #region Title
+        private string _Title;
+        [Required(ErrorMessage = "Please enter title")]
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+
+
+    }
+}
---
+++ beta/Views/HostGameView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace beta.Views
+{
+    /// <summary>
+    /// Interaction logic for HostGameView.xaml
+    /// </summary>
+    public partial class HostGameView : UserControl
+    {
+        public HostGameView() => InitializeComponent();
+    }
+}
