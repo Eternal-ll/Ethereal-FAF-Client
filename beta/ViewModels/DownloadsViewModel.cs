4fb0b4d843388a3e1971b298845346364c13bcff
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2022-04-05 15:57:29 +0500

    Some stylization for DownloadsView

--- beta/ViewModels/DownloadsViewModel.cs
+++ beta/ViewModels/DownloadsViewModel.cs
@@ -1,5 +1,4 @@
-﻿using beta.Infrastructure.Services;
-using beta.Infrastructure.Services.Interfaces;
+﻿using beta.Infrastructure.Services.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using System.Collections.ObjectModel;
 using System.Windows.Data;
@@ -13,9 +12,25 @@ namespace beta.ViewModels
         public DownloadsViewModel()
         {
             DownloadService = App.Services.GetService<IDownloadService>();
+
+            DownloadService.NewDownload += DownloadService_NewDownload;
+            DownloadService.DownloadEnded += DownloadService_DownloadEnded;
+            
             BindingOperations.EnableCollectionSynchronization(DownloadService.Downloads, _lock);
         }
 
+        private void DownloadService_DownloadEnded(object sender, DownloadViewModel e) => Latest = DownloadService.Latest;
+        private void DownloadService_NewDownload(object sender, DownloadViewModel e) => Latest = DownloadService.Latest;
+
         public ObservableCollection<DownloadViewModel> Downloads => DownloadService.Downloads;
+
+        #region Latest
+        private DownloadViewModel _Latest;
+        public DownloadViewModel Latest
+        {
+            get => _Latest;
+            set => Set(ref _Latest, value);
+        }
+        #endregion
     }
 }
