ca68fa23092b9dcc929ef37387e103256ee9404e
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2022-04-05 08:27:17 +0500

    Added downloads view/viewModel

--- beta/ViewModels/DownloadsViewModel.cs
+++ beta/ViewModels/DownloadsViewModel.cs
@@ -1,7 +1,21 @@
-﻿namespace beta.ViewModels
+﻿using beta.Infrastructure.Services;
+using beta.Infrastructure.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+
+namespace beta.ViewModels
 {
     internal class DownloadsViewModel : Base.ViewModel
     {
+        private readonly IDownloadService DownloadService;
+        private readonly object _lock = new();
+        public DownloadsViewModel()
+        {
+            DownloadService = App.Services.GetService<IDownloadService>();
+            BindingOperations.EnableCollectionSynchronization(DownloadService.Downloads, _lock);
+        }
 
+        public ObservableCollection<DownloadViewModel> Downloads => DownloadService.Downloads;
     }
 }
