5d9e1a2906179ba788f6a90b8c3c6907555221da
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2022-04-07 23:14:16 +0500

    Fix deadlock

--- beta/Infrastructure/Services/NotificationService.cs
+++ beta/Infrastructure/Services/NotificationService.cs
@@ -1,6 +1,4 @@
-﻿using beta.Infrastructure.Services.Interfaces;
-using beta.ViewModels;
-using Microsoft.Extensions.DependencyInjection;
+﻿using beta.ViewModels;
 using ModernWpf.Controls;
 using System.Threading;
 using System.Threading.Tasks;
@@ -31,14 +29,14 @@ namespace beta.Infrastructure.Services
 
         public async Task ShowPopupAsync(string text)
         {
-            await WaitForComplete();
+            //await WaitForComplete();
             ContentDialog.Dispatcher.Invoke(() =>
             {
                 ContentDialog.PrimaryButtonText = null;
                 ContentDialog.SecondaryButtonText = null;
                 ContentDialog.CloseButtonText = "OK";
                 ContentDialog.Content = text;
-                await ContentDialog.ShowAsync();
+                ContentDialog.ShowAsync();
             });
         }
         public void ShowPopup(string text)
@@ -51,7 +49,7 @@ namespace beta.Infrastructure.Services
 
         public async Task ShowPopupAsync(object model)
         {
-            await WaitForComplete();
+            //await WaitForComplete();
             ContentDialog.Dispatcher.Invoke(() =>
             {
                 ContentDialog = new();
@@ -65,14 +63,14 @@ namespace beta.Infrastructure.Services
 
         public async Task<ContentDialogResult> ShowDialog(string text)
         {
-            await WaitForComplete();
+            ContentDialog = new();
             ContentDialog.Content = text;
             return await ContentDialog.ShowAsync();
         }
 
         public async Task<ContentDialogResult> ShowDialog(object model, string primary = null, string secondary = null, string close = null)
         {
-            await WaitForComplete();
+            ContentDialog = new();
             ContentDialog.Content = model;
 
             ContentDialog.PrimaryButtonText = primary;
@@ -104,7 +102,7 @@ namespace beta.Infrastructure.Services
 
         public async Task<ContentDialogResult> ShowDialog(string text, string primary = null, string secondary = null, string close = null)
         {
-            await WaitForComplete();
+            ContentDialog = new();
             ContentDialog.Content = text;
 
             ContentDialog.PrimaryButtonText = primary;
@@ -116,7 +114,7 @@ namespace beta.Infrastructure.Services
 
         public async Task<bool> ShowDownloadDialog(DownloadViewModel model, string close = null)
         {
-            await WaitForComplete();
+            ContentDialog = new();
             model.Completed += Download_Completed;
 
             ContentDialog.PreviewKeyDown += HideEscapeKey;
