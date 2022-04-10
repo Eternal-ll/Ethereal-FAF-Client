9c348078645e7838262e74dc1692bd2818f660a6
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2022-04-10 18:12:25 +0500

    For replay server

---
+++ beta/Infrastructure/Services/ReplayServerService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+namespace beta.Infrastructure.Services
+{
+    internal class ReplayServerService : Interfaces.IReplayServerService
+    {
+        public Task Close()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Restart()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Start()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    internal class ReplayRecorders
+    {
+
+    }
+}
