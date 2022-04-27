c0663d355cfd42f3ad365aa4baccabe55bd2e16d
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2022-04-27 19:46:59 +0500

    Added self serfice for authorized user instance

---
+++ beta/Infrastructure/Services/Interfaces/ISelfService.cs
@@ -0,0 +1,11 @@
+﻿using beta.Models.Server;
+using System;
+
+namespace beta.Infrastructure.Services.Interfaces
+{
+    public interface ISelfService
+    {
+        public event EventHandler<PlayerInfoMessage> SelfUpdated;
+        public PlayerInfoMessage Self { get; }
+    }
+}
---
+++ beta/Infrastructure/Services/SelfService.cs
@@ -0,0 +1,36 @@
+﻿using beta.Infrastructure.Services.Interfaces;
+using beta.Models.Server;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace beta.Infrastructure.Services
+{
+    internal class SelfService : ISelfService
+    {
+        public PlayerInfoMessage Self { get; private set; }
+
+        public event EventHandler<PlayerInfoMessage> SelfUpdated;
+        
+        private readonly IPlayersService PlayersService;
+        
+        public SelfService()
+        {
+            PlayersService = App.Services.GetService<IPlayersService>();
+            PlayersService.SelfReceived += PlayersService_SelfReceived;
+            PlayersService.PlayerUpdated += PlayersService_PlayerUpdated;
+        }
+
+        private void PlayersService_SelfReceived(object sender, PlayerInfoMessage e)
+        {
+            Self = e;
+            SelfUpdated?.Invoke(this, e);
+        }
+
+        private void PlayersService_PlayerUpdated(object sender, PlayerInfoMessage e)
+        {
+            if (e.RelationShip != Models.Server.Enums.PlayerRelationShip.Me) return;
+            Self = e;
+            SelfUpdated?.Invoke(this, e);
+        }
+    }
+}
