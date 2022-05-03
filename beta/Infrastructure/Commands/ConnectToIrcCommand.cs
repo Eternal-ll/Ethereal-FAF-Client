7c8cd565dccd94fb81c1641c99828d89d7999a4d
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2022-05-03 18:13:55 +0500

    Hotifx irc connection

--- beta/Infrastructure/Commands/ConnectToIrcCommand.cs
+++ beta/Infrastructure/Commands/ConnectToIrcCommand.cs
@@ -1,15 +1,31 @@
 ﻿using beta.Infrastructure.Services.Interfaces;
 using beta.Properties;
 using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
 
 namespace beta.Infrastructure.Commands
 {
     internal class ConnectToIrcCommand : Base.Command
     {
         private readonly IIrcService IrcService;
-        public ConnectToIrcCommand() => IrcService = App.Services.GetService<IIrcService>();
+        private readonly INotificationService NotificationService;
+        public ConnectToIrcCommand()
+        {
+            IrcService = App.Services.GetService<IIrcService>();
+            NotificationService = App.Services.GetService<INotificationService>();
+        }
         public override bool CanExecute(object parameter) => IrcService.State == Models.Enums.IrcState.Disconnected;
-        public override void Execute(object parameter) => IrcService.Authorize(Settings.Default.PlayerNick, Settings.Default.irc_password);
+        public override void Execute(object parameter)
+        {
+            Task.Run(() => IrcService.Authorize(Settings.Default.PlayerNick, Settings.Default.irc_password))
+                .ContinueWith(task =>
+                {
+                    if (task.IsFaulted)
+                    {
+                        NotificationService.ShowExceptionAsync(task.Exception);
+                    }
+                });
+        }
     }
     internal class DisconnectFromIrcCommand : Base.Command
     {
