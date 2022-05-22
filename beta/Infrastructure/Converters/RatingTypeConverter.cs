cd853711f44d2ad73e6ef5d064ab83d86ae89334
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2022-05-22 17:15:36 +0500

    Added rating type converter with extension

---
+++ beta/Infrastructure/Converters/RatingTypeConverter.cs
@@ -0,0 +1,22 @@
+﻿using beta.Models.Extensions;
+using beta.Models.Server.Enums;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace beta.Infrastructure.Converters
+{
+    internal class RatingTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not RatingType ratingType) return "Unknown type";
+            return ratingType.FormatToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
