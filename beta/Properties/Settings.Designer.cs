e645d2a30796fbe928e47bd805743ebc8be88849
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2022-06-15 18:49:25 +0500

    Remade window savings

--- beta/Properties/Settings.Designer.cs
+++ beta/Properties/Settings.Designer.cs
@@ -347,5 +347,29 @@ namespace beta.Properties {
                 this["IsTrailerSoundsOn"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1024, 768")]
+        public global::System.Windows.Size WindowSize {
+            get {
+                return ((global::System.Windows.Size)(this["WindowSize"]));
+            }
+            set {
+                this["WindowSize"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Windows.Point WindowLocation {
+            get {
+                return ((global::System.Windows.Point)(this["WindowLocation"]));
+            }
+            set {
+                this["WindowLocation"] = value;
+            }
+        }
     }
 }
--- beta/Views/Windows/NewWindow.xaml.cs
+++ beta/Views/Windows/NewWindow.xaml.cs
@@ -11,51 +11,28 @@ namespace beta.Views.Windows
         public NewWindow()
         {
             InitializeComponent();
+            Loaded += OnWindowLoaded;
+            Closing += OnWindowClosing;
+        }
 
-            //var top = Settings.Default.Top;
-            //var left = Settings.Default.Left;
-            //var height = Settings.Default.Height;
-            //var width = Settings.Default.Width;
-
-            //if (top < 0 || top > 1000) top = 0;
-            //if(left < 0 || left > 1000) left = 0;
-            //if (height < 0 || height > 1000) height = 1000;
-            //if (width < 0 || width > 1000) width = 1000;
-
-            //Top = top;
-            //Left = left;
-            //Height = height;
-            //Width = width;
-
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
             if (Settings.Default.IsWindowMaximized)
             {
                 WindowState = WindowState.Maximized;
+                return;
             }
-
-            Closing += OnWindowClosing;
+            Settings.Default.WindowLocation = RestoreBounds.Location;
+            Settings.Default.WindowSize = RestoreBounds.Size;
         }
-        public bool ShutdownApplication { get; set; } = true;
 
         private void OnWindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            if (WindowState == WindowState.Maximized)
-            {
-                Settings.Default.Top = RestoreBounds.Top;
-                Settings.Default.Left = RestoreBounds.Left;
-                Settings.Default.Height = RestoreBounds.Height;
-                Settings.Default.Width = RestoreBounds.Width;
-                Settings.Default.IsWindowMaximized = true;
-            }
-            else
-            {
-                Settings.Default.Top = Top;
-                Settings.Default.Left = Left;
-                Settings.Default.Height = Height;
-                Settings.Default.Width = Width;
-                Settings.Default.IsWindowMaximized = false;
-            }
-            if (!ShutdownApplication)
-                App.Current.Shutdown();
+            Settings.Default.WindowLocation = RestoreBounds.Location;
+            Settings.Default.WindowSize = RestoreBounds.Size;
+            Settings.Default.IsWindowMaximized = WindowState == WindowState.Maximized;
+
+            App.Current.Shutdown();
         }
     }
 }
