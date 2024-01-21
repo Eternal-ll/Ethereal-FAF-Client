7abc6edcdb542c52e56aef680f5510a028c50fa8
Aigiz Iskuzhin <aigiz.iskuzhin@yandex.ru>
2024-01-21 17:57:07 +0500

    Added type of update for client

--- Ethereal.FAF.UI.Client/Infrastructure/Updater/UpdateInfo.cs
+++ Ethereal.FAF.UI.Client/Infrastructure/Updater/UpdateInfo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Ethereal.FAF.UI.Client.Models.Update;
+using System;
+using System.Text.Json.Serialization;
 
 namespace Ethereal.FAF.UI.Client.Infrastructure.Updater
 {
@@ -6,8 +8,10 @@ namespace Ethereal.FAF.UI.Client.Infrastructure.Updater
     {
         public Uri Url { get; set; } = null!;
         public string Version { get; set; } = null!;
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public UpdateChannel Channel { get; set; }
+        public UpdateType Type { get; set; }
         public DateTimeOffset ReleaseDate { get; set; }
-        public bool IsMandatory { get; set; }
         public bool TryGetVersion(out Version version) => System.Version.TryParse(Version, out version);
     }
 }
---
+++ Ethereal.FAF.UI.Client/Models/Update/UpdateType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ethereal.FAF.UI.Client.Models.Update
+{
+    [Flags]
+    public enum UpdateType
+    {
+        /// <summary>
+        /// Usual update
+        /// </summary>
+        Normal = 1 << 0,
+        /// <summary>
+        /// Critical update
+        /// </summary>
+        Critical = 1 << 1,
+        /// <summary>
+        /// Mandatory update
+        /// </summary>
+        Mandatory = 1 << 2,
+    }
+}
