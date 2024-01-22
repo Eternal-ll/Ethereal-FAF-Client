448769cbaeef31afa632b6227935dec861315f2f
Eternal- <aigiz.iskuzhin@yandex.ru>
2024-01-22 18:20:24 +0500

    Refactoring of interaction with FA mod

---
+++ Ethereal.FA.Mod/LuaTableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using NLua;
+
+namespace Ethereal.FA.Mod
+{
+    internal static class LuaTableExtensions
+    {
+        public static List<string> ToList(this LuaTable table)
+        {
+            var data = new List<string>();
+            foreach (var item in table.Values)
+            {
+                data.Add(item.ToString());
+            }
+            return data;
+        }
+    }
+}
