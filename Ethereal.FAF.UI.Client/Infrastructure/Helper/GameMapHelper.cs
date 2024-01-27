44a65d6580e0fd1bc4aa6bb3c6049e7d437e69f1
Eternal- <aigiz.iskuzhin@yandex.ru>
2024-01-27 12:23:04 +0500

    Added custom map name parser

---
+++ Ethereal.FAF.UI.Client/Infrastructure/Helper/GameMapHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Ethereal.FAF.UI.Client.Infrastructure.Helper
+{
+    public readonly record struct GameMapInfo(string Raw, string Name, string Version);
+    internal static class GameMapHelper
+    {
+        public static Regex CommonNamePattern = new("(.*).v(\\d*)");
+
+        public static GameMapInfo Parse(string name)
+        {
+            var result = CommonNamePattern.Match(name);
+            if (result.Groups.Count == 3)
+            {
+                return new(name, result.Groups[1].Value, result.Groups[3].Value);
+            }
+            return new(name, name, null);
+        }
+    }
+}
