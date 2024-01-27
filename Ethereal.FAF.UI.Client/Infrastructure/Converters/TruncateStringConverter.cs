75c56339f36f1c4b63260957088c70a63ff92b9b
Eternal- <aigiz.iskuzhin@yandex.ru>
2024-01-27 12:19:47 +0500

    Added converter for truncating strings on view

---
+++ Ethereal.FAF.UI.Client/Infrastructure/Converters/TruncateStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using Humanizer;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Ethereal.FAF.UI.Client.Infrastructure.Converters
+{
+    internal class TruncateStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not string text) return value;
+            var maximum = int.TryParse(parameter?.ToString(), out var max) ? max : 50;
+            return text.Truncate(maximum);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
