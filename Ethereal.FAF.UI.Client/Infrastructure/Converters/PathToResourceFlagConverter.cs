007ce7b9b732b7efa196bea52218136509d6a412
Eternal- <aigiz.iskuzhin@yandex.ru>
2024-02-02 19:49:26 +0500

    Static bitmap image initialization for flags

--- Ethereal.FAF.UI.Client/Infrastructure/Converters/PathToResourceFlagConverter.cs
+++ Ethereal.FAF.UI.Client/Infrastructure/Converters/PathToResourceFlagConverter.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
+using System.Windows.Media.Imaging;
 
 namespace Ethereal.FAF.UI.Client.Infrastructure.Converters
 {
     public class PathToResourceFlagConverter : IValueConverter
     {
+        private const string DefaultFlag = "earth";
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => $"/Resources/Images/Flags/{value}.png";
+        {
+            var resource = Application.Current
+                .TryFindResource($"CountryFlagBitmapImage_{value.ToString().ToLower()}") ??
+                Application.Current.TryFindResource(DefaultFlag);
+            if (resource is BitmapImage image)
+            {
+                return image;
+            }
+            return null;
+        }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
