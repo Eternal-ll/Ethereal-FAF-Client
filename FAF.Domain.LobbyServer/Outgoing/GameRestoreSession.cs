906adcd3748b68486bdeddb3c49f1d68724733d9
Eternal- <aigiz.iskuzhin@yandex.ru>
2024-02-04 14:03:24 +0500

    hotfix missing restore game session command

---
+++ FAF.Domain.LobbyServer/Outgoing/GameRestoreSession.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FAF.Domain.LobbyServer.Outgoing
+{
+    public class GameRestoreSession : OutgoingCommand
+    {
+        public GameRestoreSession(long uid) : base("restore_game_session")
+        {
+            GameId = uid;   
+        }
+        [JsonPropertyName("game_id")]
+        public long GameId { get; }
+    }
+}
